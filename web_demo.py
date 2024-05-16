6ea5a2c477445c7d5c549b0cd4241b96a925ea23
yangchanghui <71805205+Yang-Changhui@users.noreply.github.com>
2024-05-16 15:59:43 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -37,7 +37,7 @@ import os
 # if not os.path.exists('/home/xlab-app-center/model/yangchanghui/civil-exam-internlm2-chat-7B'):
 from modelscope.hub.snapshot_download import snapshot_download
 model_dir = snapshot_download('yangchanghui/civil-exam-internlm2-chat-7B', cache_dir='/home/xlab-app-center/model/')
-
+print(model_dir)
 @dataclass
 class GenerationConfig:
     # this config is used for chat to provide more diversity
@@ -187,10 +187,10 @@ def on_btn_click():
 @st.cache_resource
 def load_model():
     
-    model = (AutoModelForCausalLM.from_pretrained('/home/xlab-app-center/model/yangchanghui/civil-exam-internlm2-chat-7B',
+    model = (AutoModelForCausalLM.from_pretrained(model_dir,
                                                   trust_remote_code=True).to(
                                                       torch.bfloat16).cuda())
-    tokenizer = AutoTokenizer.from_pretrained('/home/xlab-app-center/model/yangchanghui/civil-exam-internlm2-chat-7B',
+    tokenizer = AutoTokenizer.from_pretrained(model_dir,
                                               trust_remote_code=True)
     return model, tokenizer
 
